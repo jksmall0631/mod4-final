2584226d15da953ab30c2fc444e3c02d7b1c317a
Joshua Small <jksmall0631@gmail.com>
2017-04-13 19:07:03 -0600

    commit for deploy to heroku

--- tests/unit-test.js
+++ tests/unit-test.js
@@ -1,8 +1,21 @@
 const chai = require('chai');
 const expect = chai.expect;
+const assert = chai.assert;
 
-describe('App', function(){
+describe('App', () => {
   it('does things!', function(){
     expect('cats').to.equal('cats');
   });
 });
+
+describe('countItems', () => {
+  it('returns an object with the count of each cleanliness type', () => {
+    const array = [
+      {id:1, name:'dead rat', reason: 'don\'t want to touch it', clean: 'rancid'},
+      {id:2, name:'hoopdie', reason: 'dope old car', clean: 'dusty'}
+    ]
+    const count = {sparkling:5, dusty:3, rancid:1};
+    
+    assert.equal(countItems(array), count)
+  })
+})
